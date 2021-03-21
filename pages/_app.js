073c9b35aafddcf0506ce364ef1e1a21cfa7fac2
Tim Eitel <timeitel@outlook.com>
2021-03-21 13:39:18 +0800

    add: initial commit

---
+++ pages/_app.js
@@ -0,0 +1,44 @@
+import React from "react";
+import App from "next/app";
+import Head from "next/head";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "../styles/tailwind.css";
+
+export default class MyApp extends App {
+  componentDidMount() {
+    let comment = document.createComment(`
+
+`);
+    document.insertBefore(comment, document.documentElement);
+  }
+  static async getInitialProps({ Component, router, ctx }) {
+    let pageProps = {};
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+
+    return { pageProps };
+  }
+  render() {
+    const { Component, pageProps } = this.props;
+
+    const Layout = Component.layout || (({ children }) => <>{children}</>);
+
+    return (
+      <React.Fragment>
+        <Head>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+          />
+          <title>Notus NextJS by Creative Tim</title>
+          <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
+        </Head>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </React.Fragment>
+    );
+  }
+}
