0e2209a589c5fae01183ed4412d24a3303a1f6e1
Tim Eitel <timeitel@outlook.com>
2021-03-23 18:57:09 +0800

    wip: update copy

--- components/OnlineOrder/OnlineOrder.js
+++ components/OnlineOrder/OnlineOrder.js
@@ -7,18 +7,35 @@ const Form = styled.form`
   justify-content: center;
   align-items: flex-start;
 
-  /* input {
+  input {
     border: 1px solid black;
     border-radius: 4px;
     margin-bottom: 12px;
     height: 40px;
+    padding: 2px 8px;
   }
 
   select {
     border-radius: 4px;
     height: 40px;
     margin-bottom: 12px;
-  } */
+  }
+`;
+
+const FormGroup = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-end;
+  width: 100%;
+
+  label {
+    white-space: nowrap;
+  }
+
+  input {
+    width: 80px;
+  }
 `;
 
 export default function OnlineOrder() {
@@ -28,24 +45,30 @@ export default function OnlineOrder() {
   return (
     <>
       <Form onSubmit={handleSubmit(onSubmit)}>
-        <label>Name</label>
-        <input name="name" ref={register({ required: true })} />
+        <h4 className="font-bold mb-8">
+          Order by Monday, March 29, for Wednesday, March 24 delivery
+        </h4>
+        <FormGroup>
+          <label className="text-lg font-medium mr-4">
+            Chinese BBQ Pork served with hainan rice
+          </label>
+          <input type="number" min="0" max="100" />
+        </FormGroup>
 
-        <label>Email</label>
-        <input name="email" ref={register({ required: true })} />
-
-        <label>Mobile</label>
-        <input name="mobile" ref={register({ required: true })} />
-
-        <label>Dishes</label>
-        <select name="dishes" ref={register}>
-          <option value="0">Chinese BBQ Pork served with Hainan rice</option>
-          <option value="1">Roast chicken served with Hainan rice</option>
-        </select>
+        <FormGroup>
+          <label className="text-lg font-medium mr-4">
+            Roast chicken served with hainan rice
+          </label>
+          <input type="number" min="0" max="100" />
+        </FormGroup>
 
         {errors.exampleRequired && <span>This field is required</span>}
 
-        <input type="submit" />
+        <input
+          className="flex cursor-pointer ml-auto items-center whitespace-nowrap text-white font-bold px-6 rounded outline-none focus:outline-none mr-1 mb-1 bg-blueGray-400 active:bg-blueGray-500 uppercase text-sm shadow hover:shadow-lg ease-linear transition-all duration-150"
+          type="submit"
+          value="Checkout"
+        />
       </Form>
     </>
   );
