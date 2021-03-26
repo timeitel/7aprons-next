2f743c3d0230a6c19a2716b68916401727a1abd2
timeitel <timeitel@outlook.com>
2021-03-26 12:33:02 +0000

    Deploying to gh-pages from @ timeitel/7aprons-next@9c4e5bfe6ea8655fc1fca0de43012f8e0c431cc0 🚀

--- _next/static/4btUkzt4wVf-loVgpBxwm/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-adf26175b2e9e92b7b87.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-cd9108d203e461ed3ead.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
