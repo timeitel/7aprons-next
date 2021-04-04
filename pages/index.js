e4b93f391eb13a12c19249bde344126853f15cf3
Tim Eitel <timeitel@outlook.com>
2021-04-04 20:53:56 +0800

    update: remove landing svg

--- pages/index.js
+++ pages/index.js
@@ -77,25 +77,6 @@ export default function Index() {
         id="about"
         className="pt-48 md:pt-40 pb-16 md:pb-32 relative bg-blueGray-100"
       >
-        <div
-          className="-mt-20 top-0 bottom-auto left-0 right-0 w-full absolute h-20"
-          style={{ transform: "translateZ(0)" }}
-        >
-          <svg
-            className="absolute bottom-0 overflow-hidden"
-            xmlns="http://www.w3.org/2000/svg"
-            preserveAspectRatio="none"
-            version="1.1"
-            viewBox="0 0 2560 100"
-            x="0"
-            y="0"
-          >
-            <polygon
-              className="text-blueGray-100 fill-current"
-              points="2560 0 2560 100 0 100"
-            ></polygon>
-          </svg>
-        </div>
         <div className="container mx-auto">
           <div className="flex flex-wrap items-center">
             <div className="w-10/12 md:w-6/12 lg:w-4/12 md:px-4 mr-auto ml-auto -mt-32">
