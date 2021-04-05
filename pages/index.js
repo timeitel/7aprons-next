61a7b0f7e7e2ca4a351b83c45b8bcd9d1fbef65d
Tim Eitel <timeitel@outlook.com>
2021-04-05 08:25:58 +0800

    update: remove svg

--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,7 @@ import Footer from "components/Footers/Footer.js";
 import OrderOnline from "../components/OnlineOrder/OnlineOrder";
 import { basePath } from "../next.config";
 import styled from "styled-components";
-
-const headerStyle = {
-  background: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("${basePath}/images/landing-food.jpg")`,
-  backgroundPosition: "center center",
-  backgroundSize: "cover",
-};
+import Image from "next/image";
 
 const orderSection = {
   background: `linear-gradient(rgba(0, 0, 0, 0.65), rgba(0, 0, 0, 0.65)), url("${basePath}/images/ingredients.jpg")`,
@@ -26,16 +21,29 @@ const MenuContainer = styled.div`
   }
 `;
 
+const LandingImage = styled(Image)`
+  filter: brightness(50%);
+`;
+
 export default function Index() {
   return (
     <>
       <IndexNavbar fixed />
       <section
         id="home"
-        style={headerStyle}
         className="header relative pt-16 items-center flex h-screen max-h-860-px"
       >
-        <div className="container mx-auto items-center flex flex-wrap">
+        <LandingImage
+          alt="Indonesian food"
+          src="/images/landing-food.jpg"
+          layout="fill"
+          objectFit="cover"
+          quality={50}
+          style={{
+            filter: "brightness(50%)",
+          }}
+        />
+        <div className="container z-10 mx-auto items-center flex flex-wrap">
           <div className="w-full md:w-8/12 lg:w-6/12 xl:w-6/12 px-4">
             <div className="pt-32 sm:pt-0">
               <h2 className="font-semibold text-4xl text-white">
@@ -241,26 +249,6 @@ export default function Index() {
       </section>
 
       <section className="pb-16 bg-blueGray-200 relative pt-32">
-        <div
-          className="-mt-20 top-0 bottom-auto left-0 right-0 w-full absolute h-20"
-          style={{ transform: "translateZ(0)" }}
-        >
-          <svg
-            className="absolute bottom-0 overflow-hidden"
-            xmlns="http://www.w3.org/2000/svg"
-            preserveAspectRatio="none"
-            version="1.1"
-            viewBox="0 0 2560 100"
-            x="0"
-            y="0"
-          >
-            <polygon
-              className="text-blueGray-200 fill-current"
-              points="2560 0 2560 100 0 100"
-            ></polygon>
-          </svg>
-        </div>
-
         <div className="container mx-auto">
           <div className="flex flex-wrap justify-center bg-white shadow-xl rounded-lg -mt-64 py-12 px-12 relative z-10">
             <div className="w-full text-center lg:w-8/12">
