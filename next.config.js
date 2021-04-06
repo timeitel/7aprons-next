844a00d50e0cb7ce896df581a8a90b3c618df974
Tim Eitel <timeitel@outlook.com>
2021-04-06 21:00:21 +0800

    chore: update to ts

--- next.config.js
+++ next.config.js
@@ -3,4 +3,7 @@ const basePath = process.env.NODE_ENV === "production" ? "/7aprons-next" : "";
 module.exports = {
   basePath,
   assetPrefix: `${basePath}/`,
+  future: {
+    webpack5: true,
+  },
 };
