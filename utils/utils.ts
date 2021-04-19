4972efdcaa27d4bd0e6ce3e7198daab369504bf7
Tim Eitel <timeitel@outlook.com>
2021-04-19 19:25:51 +0800

    feat: remove week 3 products

--- utils/utils.ts
+++ utils/utils.ts
@@ -5,22 +5,6 @@ export const domain =
 
 export const newOrder = [
   {
-    name: "Fried chicken in sweet, buttery sauce, served with steamed rice",
-    price: "price_1IharaDJrsoPxmlZ53xSu407",
-    quantity: 0,
-    week: 3.1,
-    orderAndDelivery:
-      "Order by Monday, April 19, for Wednesday, April 21 delivery",
-  },
-  {
-    name: "Indonesian Beef Rendang served with Turmeric Rice",
-    price: "price_1IhasADJrsoPxmlZwx8tl3px",
-    quantity: 0,
-    week: 3.2,
-    orderAndDelivery:
-      "Order by Monday, April 19, for Wednesday, April 21 delivery",
-  },
-  {
     name: "Assam Fish served with Steamed Rice",
     price: "price_1IhapyDJrsoPxmlZaJpFWcwR",
     quantity: 0,
