22a7eb75df5b250c536493ab98cf26bf3b79d86c
Tim Eitel <timeitel@outlook.com>
2021-04-24 17:52:10 +0800

    feat: payments functions

--- backend/payments/index.js
+++ backend/payments/index.js
@@ -1,10 +1,9 @@
 /**
- * Responds to any HTTP request.
  *
  * @param {!express:Request} req HTTP request context.
  * @param {!express:Response} res HTTP response context.
  */
-exports.paymentIntents = (req, res) => {
-  let message = req.query.message || req.body.message || 'Hello World!';
+exports.payments = (req, res) => {
+  let message = req.query.message || req.body.message || "Hello Payments!";
   res.status(200).send(message);
 };
