e2c37e738a45477500e69f99cbad28f2f8f68c43
Tim Eitel <timeitel@outlook.com>
2021-04-26 10:26:10 +0800

    update: backend to stripe

--- backend/payments/index.js
+++ backend/payments/index.js
@@ -3,7 +3,26 @@
  * @param {!express:Request} req HTTP request context.
  * @param {!express:Response} res HTTP response context.
  */
-exports.payments = (req, res) => {
-  let message = req.query.message || req.body.message || "Hello Payments!";
-  res.status(200).send(message);
+exports.payments = async (req, res) => {
+  res.set("Access-Control-Allow-Origin", "*");
+  res.set("Access-Control-Allow-Methods", "GET, POST");
+
+  if (req.method === "OPTIONS") {
+    /* handle preflight OPTIONS request */
+    res.set("Access-Control-Allow-Methods", "GET, POST");
+    res.set("Access-Control-Allow-Headers", "Content-Type");
+    res.set("Access-Control-Max-Age", "3600");
+    return res.sendStatus(204);
+  }
+
+  const stripe = require("stripe")(process.env.STRIPE_KEY);
+
+  const intent = await stripe.paymentIntents.create({
+    amount: 1099,
+    currency: "aud",
+    // Verify your integration in this guide by including this parameter
+    metadata: { integration_check: "accept_a_payment" },
+  });
+
+  res.status(200).json({ client_secret: intent.client_secret });
 };
