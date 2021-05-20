cabef21f15f131afabda84e7ce51a4fa97a5ec2c
Tim Eitel <timeitel@outlook.com>
2021-05-20 21:34:00 +0800

    wip: webhook

--- pages/api/webhook.ts
+++ pages/api/webhook.ts
@@ -1,22 +1,41 @@
+import { buffer } from "micro";
+import Stripe from "stripe";
 import { NextApiRequest, NextApiResponse } from "next";
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+const webhookSecret = "whsec_hxWtHk1vpdhknr5c56c8Y1ni7W9d8c9u";
+const stripe = new Stripe(webhookSecret, null);
 
 /**
  *
- * @param {!express:Request} req HTTP request context.
- * @param {!express:Response} res HTTP response context.
+ * @param {!express:Request} req HTTP req context.
+ * @param {!express:Response} res HTTP res context.
  */
 export default async function webhook(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  try {
-    await runWebhook(req, res);
-  } catch (err) {
-    console.log(JSON.stringify(err));
-    res.status(500).send("Server error");
+  if (req.method === "POST") {
+    const buf = await buffer(req);
+    const sig = req.headers["stripe-signature"];
+
+    let event;
+
+    try {
+      event = stripe.webhooks.constructEvent(buf, sig, webhookSecret);
+    } catch (err) {
+      res.status(400).send(`Webhook Error: ${err.message}`);
+      return;
+    }
+
+    res.json({ received: true });
+  } else {
+    res.setHeader("Allow", "POST");
+    res.status(405).end("Method Not Allowed");
   }
 }
-
-const runWebhook = (req, res) => {
-  res.status(200).json();
-};
